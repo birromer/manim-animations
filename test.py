412e7897682b08902fbc794cf88637728276ce98
Bernardo Hummes <hummescmpa@gmail.com>
2022-01-26 12:28:36 +0100

    writing text with morphism, trying to display cat name afterwards

--- test.py
+++ test.py
@@ -28,14 +28,19 @@ class ObjectsWithArrow(Scene):
         obj_1 = Circle(radius=0.2, color=BLUE_A, fill_opacity=1)
         obj_2 = Circle(radius=0.2, color=BLUE_B, fill_opacity=1)
         arrow = Arrow(buff=0.1, stroke_width=5.0, max_tip_length_to_length_ratio=0.5)
-        arrow_label = Text("testetesteescreveaicaralho")
-        arrow_label.next_to(arrow, DOWN, buff=0.5)
+        arrow_label = Text("f").scale(0.75)
+        arrow_label.next_to(arrow, DOWN, buff=0.1)
 
         obj_1 = obj_1.next_to(arrow, LEFT)
         obj_2 = obj_2.next_to(arrow, RIGHT)
 
         self.play(Create(obj_1), Create(obj_2))
         self.wait(0.2)
-        self.play(Create(arrow))
+        self.play(Create(arrow), Write(arrow_label))
+        self.wait()
+
+        group_cat = Group(obj_1, obj_2, arrow, arrow_label)
+        text_cat = Text("This is the category of sets!")
+
+        self.play(group_cat.animate.next_to(text_cat, DOWN, buff=0.5))
         self.wait(3)
-#        self.add(obj_1, arrow, obj_2)
